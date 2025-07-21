189140f3c5a76fce020600279be9e17bfcd1f480
29Tech <243682G@mymail.nyp.edu.sg>
2025-07-21 18:35:46 +0800

    Add files via upload

---
+++ static/js/homepage.js
@@ -0,0 +1,27 @@
+// Get modal elements
+const modal = document.getElementById("modal");
+const logoCartoon = document.getElementById("logoCartoon");
+const closeBtn = document.querySelector(".close");
+
+// Show modal when logo is clicked
+logoCartoon.addEventListener("click", () => {
+  modal.style.display = "flex"; // Show the modal
+});
+
+// Close modal when the 'X' is clicked
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Close modal when clicking outside the modal content
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+
+ document.querySelector('.bi-cart').addEventListener('click', function () {
+     window.location.href = '/cart'; // Redirects to the cart page
+  });
+
